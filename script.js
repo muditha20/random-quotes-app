ec1a10ea6e92b9ae19d75966e63f28a79bab39fb
USERNAME <email-Address>
2022-09-19 23:13:41 +0530

    Download button operations are added

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ let getQuote = () => {
 };
 
 window.addEventListener('load',getQuote);
+
+//////////////////////Capture div/////////////////////////
+
+function download(url) {
+    var a = $("<a style='display:none' id='js-downloder'>")
+        .attr("href", url)
+        .attr("download", "quote.png")
+        .appendTo("body");
+
+    a[0].click();
+
+    a.remove();
+}
+
+function saveCapture(element) {
+    html2canvas(element).then(function (canvas) {
+        download(canvas.toDataURL("image/png"));
+    })
+}
+
+$('#btnDownload').click(function () {
+    var element = document.querySelector("#capture");
+    saveCapture(element)
+})
